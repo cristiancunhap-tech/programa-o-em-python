9fec4067f2e099b3b6e9822cef72754423a34bbc
crcunha <cristiancunhap@gmail.com>
2026-08-26 19:38:33 -0300

    .

---
+++ unidade 2/javascript/referencia.js
@@ -0,0 +1,15 @@
+let idade = 24;
+const CPF = "12345267";
+
+const temCopadoBrasil = false;
+
+
+const usuario = {
+    usuario: "Cristian",
+    idade: 23,
+}
+
+
+function funcaoManeira(){
+    
+}
---
+++ unidade 2/javascript/soma.js
@@ -0,0 +1,9 @@
+let numero1 = 20
+
+let numero2 = 22
+
+const soma = numero1 + numero2
+
+alert(soma)
+
+    
