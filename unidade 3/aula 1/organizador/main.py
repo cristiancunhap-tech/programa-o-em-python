2d07ecbf43b96546af663117a637ebb8ca667960
crcunha <cristiancunhap@gmail.com>
2026-09-02 20:26:54 -0300

    ;

--- unidade 3/aula 1/organizador/main.py
+++ unidade 3/aula 1/organizador/main.py
@@ -1,2 +1,14 @@
 import os
 import shutil
+
+pastas_e_arquivos = {
+    "Imagens" : ["png", "jpeg", "jpg", "webp"],
+    "Planilhas" : ["xls", "csv", "xlsx"],
+    "Documentos" : ["docx", "pdf", "txt"]
+}
+
+pasta_alvo = "./Bagunça"
+lista_arquivos = (os.listdir(pasta_alvo))
+
+for arquivo in lista_arquivos:
+    extensao = arquivo.split(".")[-1]
